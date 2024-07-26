74bb9501de364644a4061c8bec8e8d0cdae1a60e
RAMESH SINGH <ramesh.singh@tibilsolutions.com>
2024-07-26 22:36:32 +0530

    test2428

--- Utilities/readproperties.py
+++ Utilities/readproperties.py
@@ -2,9 +2,8 @@ import configparser
 import os
 import csv
 
-
 class ReadConfig:
-    config_file = os.path.join(os.path.dirname(__file__), '../Configuration/config.init')
+    config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '../Configuration/config.init'))
 
     @staticmethod
     def get_config():
@@ -21,8 +20,9 @@ class ReadConfig:
         config = ReadConfig.get_config()
         try:
             logs_dir = config['DEFAULT']['LogsDirectory']
-            print(f"Logs directory: {logs_dir}")
-            return logs_dir
+            logs_dir_abs = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', logs_dir))
+            print(f"Logs directory: {logs_dir_abs}")
+            return logs_dir_abs
         except KeyError:
             raise KeyError("LogsDirectory key is missing in the configuration file.")
 
@@ -31,19 +31,19 @@ class ReadConfig:
         config = ReadConfig.get_config()
         try:
             csv_file_path = config['DEFAULT']['CSVFilePath']
-            print(f"CSV file path: {csv_file_path}")
-            return csv_file_path
+            csv_file_path_abs = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', csv_file_path))
+            print(f"CSV file path: {csv_file_path_abs}")
+            return csv_file_path_abs
         except KeyError:
             raise KeyError("CSVFilePath key is missing in the configuration file.")
 
     @staticmethod
     def read_csv(file_path):
-        abs_path = os.path.join(os.path.dirname(__file__), '..', file_path)
-        print(f"Reading CSV file from: {abs_path}")
-        if os.path.exists(abs_path):
-            with open(abs_path, mode='r') as csvfile:
+        print(f"Reading CSV file from: {file_path}")
+        if os.path.exists(file_path):
+            with open(file_path, mode='r') as csvfile:
                 csv_reader = csv.DictReader(csvfile)
                 data = [row for row in csv_reader]
             return data
         else:
-            raise FileNotFoundError(f"CSV file not found: {abs_path}")
+            raise FileNotFoundError(f"CSV file not found: {file_path}")
